77ab27b7a5141808b8bd6e52f4a31ea4e07dca20
mcosminc <cosmin_ciovina@yahoo.com>
2023-03-11 20:19:04 +0200

    Changes

--- app/js/app.js
+++ app/js/app.js
@@ -1,10 +1,14 @@
 const btnHamburger = document.querySelector('#btnHamburger')
+const header = document.querySelector('.header')
+
 
 btnHamburger.addEventListener("click" , function() {
     if(btnHamburger.classList.contains("open")) {
         btnHamburger.classList.remove("open");
+        header.classList.remove("open");
     }   else {
         btnHamburger.classList.add("open");
+        header.classList.add("open");
     }
 
 });
