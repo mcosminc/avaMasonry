c51cb33d19fc0b83eaa27d82238058a87d2b24ed
mcosminc <cosmin_ciovina@yahoo.com>
2023-03-11 20:34:55 +0200

    Fixed menu transition

--- app/js/app.js
+++ app/js/app.js
@@ -3,11 +3,11 @@ const header = document.querySelector('.header')
 
 
 btnHamburger.addEventListener("click" , function() {
-    if(btnHamburger.classList.contains("open")) {
-        btnHamburger.classList.remove("open");
+    if(header.classList.contains("open")) {
+
         header.classList.remove("open");
     }   else {
-        btnHamburger.classList.add("open");
+
         header.classList.add("open");
     }
 
